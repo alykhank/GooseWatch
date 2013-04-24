907aee2495895c95131ee8f98c008a91656e19f0
Alykhan Kanji <github@alykhan.com>
2013-04-23 22:35:38 -0400

    Add GooseWatchViewController, UIViewController subclass.

---
+++ Goose Watch/GooseWatchViewController.h
@@ -0,0 +1,13 @@
+//
+//  GooseWatchViewController.h
+//  Goose Watch
+//
+//  Created by alykhan.kanji on 23/04/13.
+//  Copyright (c) 2013 Alykhan Kanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GooseWatchViewController : UIViewController
+
+@end
---
+++ Goose Watch/GooseWatchViewController.m
@@ -0,0 +1,38 @@
+//
+//  GooseWatchViewController.m
+//  Goose Watch
+//
+//  Created by alykhan.kanji on 23/04/13.
+//  Copyright (c) 2013 Alykhan Kanji. All rights reserved.
+//
+
+#import "GooseWatchViewController.h"
+
+@interface GooseWatchViewController ()
+
+@end
+
+@implementation GooseWatchViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
