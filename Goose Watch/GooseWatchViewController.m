ecd734fa4f7ba9799ee2d9d5842726f475014854
Alykhan Kanji <github@alykhan.com>
2013-04-23 23:17:17 -0400

    Add sightings, annotations, mapView properties and implement refreshPressed.

--- Goose Watch/GooseWatchViewController.m
+++ Goose Watch/GooseWatchViewController.m
@@ -7,12 +7,43 @@
 //
 
 #import "GooseWatchViewController.h"
+#import <MapKit/MapKit.h>
+#import "GooseWatch.h"
+#import "SightingAnnotation.h"
 
 @interface GooseWatchViewController ()
-
+@property (nonatomic, strong) NSArray *sightings;
+@property (nonatomic, strong) NSArray *annotations; // of id <MKAnnotation>
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
 @end
 
 @implementation GooseWatchViewController
+
+@synthesize sightings = _sightings;
+@synthesize annotations = _annotations;
+@synthesize mapView = _mapView;
+
+- (void)setAnnotations:(NSArray *)annotations
+{
+    if (_annotations != annotations) {
+        _annotations = annotations;
+        [self updateMapView];
+    }
+}
+
+- (void)setMapView:(MKMapView *)mapView
+{
+    if (_mapView != mapView) {
+        _mapView = mapView;
+        [self updateMapView];
+    }
+}
+
+- (void)updateMapView
+{
+    if (self.mapView.annotations) [self.mapView removeAnnotations:self.mapView.annotations];
+    if (self.annotations) [self.mapView addAnnotations:self.annotations];
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -35,4 +66,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)refreshPressed:(UIBarButtonItem *)sender {
+    self.sightings = [GooseWatch retrieveSightings];
+    NSMutableArray *annotations = [NSMutableArray arrayWithCapacity:self.sightings.count];
+    for (NSDictionary *sighting in self.sightings) {
+        [annotations addObject:[SightingAnnotation annotationForSighting:sighting]];
+    }
+    self.annotations = annotations;
+}
 @end
