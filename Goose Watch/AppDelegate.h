d2a9f1514c0e04bab4de49ba49122692074431ef
Alykhan Kanji <github@alykhan.com>
2013-04-23 22:27:33 -0400

    Initial Commit

---
+++ Goose Watch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Goose Watch
+//
+//  Created by alykhan.kanji on 23/04/13.
+//  Copyright (c) 2013 Alykhan Kanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Goose Watch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Goose Watch
+//
+//  Created by alykhan.kanji on 23/04/13.
+//  Copyright (c) 2013 Alykhan Kanji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
